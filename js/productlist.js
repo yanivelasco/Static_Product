0ce9c1fa98ae2e427b31e0b7b632b782b34d4aef
Yani <yanivlascogmail.com>
2022-09-04 22:01:06 +0200

    video2ok

--- js/productlist.js
+++ js/productlist.js
@@ -14,5 +14,21 @@ function handleProductList(data){
 }
 
 function showProduct(product){
-    
+    console.log(product);
+
+    const template = document.querySelector("#smallProductTemplate").content;
+    const copy = template.cloneNode(true);
+    copy.querySelector(".subtle").textContent =  `${product.articletype} | ${product.brandname}` 
+    copy.querySelector("h3").textContent = product.productdisplayname;
+    // if(product.soldout){
+    //     copy.querySelector("article").classList.add("soldOut");
+    // }
+    // if(product.discount){
+    //     copy.querySelector("article").classList.add("onSale");
+    //     copy.querySelector("discounted p").textContent = product.price
+    // }
+    const parent = document.querySelector("main");
+    parent.appendChild(copy);
+
+
 }
