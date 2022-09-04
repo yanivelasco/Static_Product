69573f76e3e739560ef3a26d2169954a0d1e8183
Yani <yanivlascogmail.com>
2022-09-04 23:19:32 +0200

    almost there

---
+++ js/category.js
@@ -0,0 +1,20 @@
+const url = "https://kea-alt-del.dk/t7/api/brands";
+
+fetch(url)
+  .then((res) => res.json())
+  .then((data) => handleBrandList(data));
+
+
+function handleBrandList(data) {
+  console.log(data);
+  data.forEach(showBrand);
+}
+
+function showBrand(brand) {
+  const temp = document.querySelector("template").content;
+  const clone = temp.cloneNode(true);
+  clone.querySelector("a").href = `productlist.html?brandname=${brand.brandname}`;
+  clone.querySelector("h2").textContent = brand.brandname;
+  const parent = document.querySelector("main");
+  parent.appendChild(clone);
+}
